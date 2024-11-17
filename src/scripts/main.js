5ce7281767472109741e0bfae838de045d239fb4
TargoniyAnatoliy <targoniyanatoliy@gmail.com>
2024-11-17 18:01:51 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const workersList = document.querySelector('ul');
+const workersArr = [...workersList.children];
+
+workersList.innerHTML = '';
+
+sortList(workersArr).forEach((worker) => workersList.append(worker));
+
+function getSalary(worker) {
+  const workerSalary = worker.getAttribute('data-salary').replaceAll('$', '');
+
+  return parseFloat(workerSalary.replaceAll(',', ''));
+}
+
+function sortList(arr) {
+  return arr.sort(
+    (worker1, worker2) => getSalary(worker2) - getSalary(worker1),
+  );
+}
